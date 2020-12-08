fb67ce60acf79679a283e585e2a708935f4916a4
CrunchWillSupreme <33377681+CrunchWillSupreme@users.noreply.github.com>
2020-12-08 10:09:28 -0500

    Update ProfitPrimisOpenCloseCLI.py

--- claims_reporter/app/codes/ProfitPrimisOpenCloseCLI.py
+++ claims_reporter/app/codes/ProfitPrimisOpenCloseCLI.py
@@ -27,10 +27,10 @@ def main(email_user, email_pwd, datestring, sendmail:bool=False):
         flag = False
         while not flag:
             try:    
-                recipients = ['brandon.arnold@markel.com']
+                recipients = ['USER@EMAIL.com']
                 subject = "Profit & Primis Open and Closed Claim Report"
-                body = "Hello Brandon,\n\nAttached you will find the Profit & Primis Opened and Closed Claims Report for {end_date:%B %Y}.  Please let me know if you have any questions.\n\nThanks,\nWill Han".format(end_date=end_date)
-                ppattach = [r'\\MKLFILE\CLAIMS\corpfs06-filedrop\ClaimsReporting\Compliance\Profit & Primis Opened and Closed Claim Report - {end_date:%B %Y}.xlsx'.format(end_date=end_date)]
+                body = "Hello USER,\n\nAttached you will find the Profit & Primis Opened and Closed Claims Report for {end_date:%B %Y}.  Please let me know if you have any questions.\n\nThanks,\nWill Han".format(end_date=end_date)
+                ppattach = [r'\\PATH\TO\FILE\Profit & Primis Opened and Closed Claim Report - {end_date:%B %Y}.xlsx'.format(end_date=end_date)]
                 email( recipients, subject, body, ppattach, email_user, email_pwd)
                 print('Email Sent!')
                 flag = True
@@ -38,7 +38,7 @@ def main(email_user, email_pwd, datestring, sendmail:bool=False):
                 time.sleep(5)
 	
 def get_json():
-	with open(r'\\MKLFILE\CLAIMS\corpfs06-filedrop\ClaimsReporting\Projects\Monthly_Cognos_Reports\Files\schedule.json') as f:
+	with open(r'\\PATH\TO\FILE\schedule.json') as f:
 		schedule = json.loads(f.read())
 	return schedule
 	
@@ -50,16 +50,16 @@ def get_open_and_closed_dates(schedule, dates_for=dt.date.today()):
     return start_date, end_date
 
 def read_queries(start_date, end_date):
-    with open(r'P:\GitHub\claims_reporter\app\codes\SQL\ProfitPrimis\results_temp.sql') as r:
+    with open(r'PATH\TO\FILE\results_temp.sql') as r:
         temp = r.read()
-    with open(r'P:\GitHub\claims_reporter\app\codes\SQL\ProfitPrimis\final_query.sql') as f:
+    with open(r'PATH\TO\FILE\final_query.sql') as f:
         finalquery = f.read().format(start_date=start_date, end_date=end_date)
     return temp, finalquery
 	
 def runqueryandexcel(source, temp, finalquery, end_date):
     #Create Connection
     print('Creating server connection...')
-    DataLakeserver = 'VA1-PCORSQL210,21644'
+    DataLakeserver = '[SERVER_ADDRESS]'
     driver = '{SQL Server}'    # Driver you need to connect to the database
     port = '1433'
     DataLakecnn = pyodbc.connect('DRIVER='+driver+';PORT='+port+';SERVER='+DataLakeserver)
@@ -72,7 +72,7 @@ def runqueryandexcel(source, temp, finalquery, end_date):
     print(source + " results stored!")                               
     rngoutput = output.values.tolist()
     print('Sheet added!\nRetrieving TEMPLATE workbook')
-    wb = load_workbook(r"\\MKLFILE\CLAIMS\corpfs06-filedrop\ClaimsReporting\Projects\Monthly_Cognos_Reports\TEMPLATES\TEMPLATE - ProfitPrimisOpenedClosedClaim.xlsx")
+    wb = load_workbook(r"\\PATH\TO\FILE\TEMPLATE - ProfitPrimisOpenedClosedClaim.xlsx")
     print("TEMPLATE workbook retrieved! \nPasting "+source+" output onto Excel sheet..")
     ws=wb.get_sheet_by_name('Sheet1')
     for row_num, row in enumerate(rngoutput):
@@ -80,7 +80,7 @@ def runqueryandexcel(source, temp, finalquery, end_date):
             ws.cell(row=row_num+2,column=col_num+1).value=val #python is zero-indexed, openpyxl is 1-indexed
     print(source+" output pasted onto sheet!")                    
     print('Sheet name updated! \nSaving workbook...')
-    wb.save(r'\\MKLFILE\CLAIMS\corpfs06-filedrop\ClaimsReporting\Compliance\Profit & Primis Opened and Closed Claim Report - {end_date:%B %Y}.xlsx'.format(end_date=end_date))
+    wb.save(r'\\PATH\TO\FILE\Profit & Primis Opened and Closed Claim Report - {end_date:%B %Y}.xlsx'.format(end_date=end_date))
     print('Workbook Saved!')
     return
 
@@ -89,7 +89,7 @@ def email(to, subject, text, ppattach, email_user, email_pwd, cc=None):
     msg = MIMEMultipart()
     msg['From'] = email_user
     msg['To'] = ", ".join(to)
-    bcc = ['whan@markelcorp.com']
+    bcc = ['USER@EMAIL.com']
     msg['Subject'] = subject
     print('Adding body of message...')
     msg.attach(MIMEText(text))
@@ -115,135 +115,6 @@ def date_format(datestring):
     x = datestring
     x = dt.datetime.strptime(x, '%m/%Y')
     return x
-		
-#def finalQuery(start_date, end_date):
-#    return """SELECT * FROM ##TEMP_RESULTS
-#    WHERE 
-#    ([Claim Feature Closed Date] BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d}' OR [Claim Folder Closed Date] BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d}')
-#    
-#    OR
-#    ([Claim Feature Opened Date] BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d}' OR [Claim Folder Opened Date] BETWEEN '{start_date:%Y-%m-%d}' AND '{end_date:%Y-%m-%d}')
-#      
-#    OR [ITD Expense] > 0
-#    OR (
-#    ([Folder Status] NOT IN ('Closed', 'Void') AND [State Report Indicator] IS NOT NULL) OR ([Feature Status] NOT IN ('Closed', 'Void') AND [State Report Indicator] IS NOT NULL)
-#    )
-#    OR (
-#    ([Folder Status] NOT IN ('Closed', 'Void') AND [NPDB Report Indicator] IS NOT NULL) OR ([Feature Status] NOT IN ('Closed', 'Void') AND [NPDB Report Indicator] IS NOT NULL)
-#    )
-#    
-#    ORDER BY [Legacy System], [Claim Number] asc
-#    """.format(start_date=start_date, end_date=end_date)
-#
-#def tempQuery():
-#	return """IF OBJECT_ID('tempdb..##TEMP_RESULTS') IS NOT NULL DROP TABLE ##TEMP_RESULTS
-#		SELECT * 
-#		INTO ##TEMP_RESULTS
-#		FROM (
-#		select DISTINCT
-#		'PRIMIS' AS 'Legacy System'
-#		,ADJ.B27_NAME1 AS 'Examiner Name'
-#		,AT.D76_DESCRIPTION AS 'File Type'
-#		,CC.B69_CLAIM_OCCUR AS 'Claim Number'
-#		,CC.D87_ACC_STATE AS 'Loss State'
-#		,CASE CC.STATE_REPORTING
-#			WHEN '1' THEN 'Yes'
-#			WHEN  '2' THEN 'No'
-#				ELSE NULL END AS 'State Report Indicator'
-#		,CASE CC.NPDB_REPORTING
-#			WHEN '1' THEN 'Yes'
-#			WHEN '2' THEN 'No'
-#				ELSE NULL END AS 'NPDB Report Indicator'
-#		,CASE CC.E87_STATUS 
-#			WHEN 0 THEN 'Open'
-#			WHEN 1 THEN 'New'
-#			WHEN 2 THEN 'Closed'
-#			WHEN 3 THEN 'Open for Recovery'
-#			WHEN 4 THEN 'Void' ELSE CAST(CC.E87_STATUS AS varchar) END AS 'Folder Status'
-#		,CR.[F04_DI_LOSS_PAID] AS 'ITD Loss'
-#		,CASE WHEN CR.B69_CLAIM_OCCUR IS NULL THEN NULL ELSE CONCAT(CR.B69_CLAIM_OCCUR,'-',CR.U10_CLMNUM) END AS 'Claim Feature Number'
-#		,NA1.B27_NAME1 AS 'Issuing Company'
-#		,CONCAT(CC.A00_PNUM,'-',CC.A06_EDITION) AS 'Policy Number/Edition'
-#		,CR.C07_LIMIT_3 AS 'Occurrence Limit'
-#		,CR.B85_DED_AMT AS 'Deductible'
-#		,NA2.B27_NAME1 AS 'Insured Name'
-#		,NA2.B28_ADDR1 AS 'Insured Address 1'
-#		,NA2.B28_ADDR2 AS 'Insured Address 2'
-#		,NA2.B30_CITY AS 'Insured City'
-#		,NA2.B31_STATE AS 'Insured State'
-#		,NA2.B32_ZIP AS 'Insured Zip'
-#		,PC.A02_RATING_STATE AS 'Risk State'
-#		,CLMT.B27_NAME1 AS 'Claimant Name'
-#		,CONCAT(CLMT.B28_ADDR1,' ',CLMT.B28_ADDR2) AS 'Claimant Address'
-#		,CLMT.B30_CITY AS 'Claimant City'
-#		,CLMT.B31_STATE AS 'Claimant State'
-#		,CLMT.B32_ZIP AS 'Claimant Zip'
-#		,CC.B70_LOSS_DATE AS 'Loss Date'
-#		,CC.G36_CLAIMS_MADE AS 'Claims Made Date'
-#		,CC.D43_REPORTED_DATE AS 'Reported Date'
-#		,CC.H53_NDATE AS 'Claim Folder Opened Date'
-#		,CASE WHEN CC.E87_STATUS = 3 THEN CC.E87_STATUS_DATE END AS 'Claim Folder Re-Opened Date'
-#		,CASE WHEN CC.E87_STATUS = 2 THEN CC.E87_STATUS_DATE END AS 'Claim Folder Closed Date'
-#		,CR.H53_NDATE AS 'Claim Feature Opened Date'
-#		,CASE WHEN CR.E87_STATUS = 2 THEN CR.E87_STATUS_DATE END AS 'Claim Feature Closed Date'
-#		,CC.[ERROR_BEGIN_DATE] AS 'Error Begin Date'
-#		,CC.[ERROR_END_DATE] AS 'Error End Date'
-#		,CASE CR.E87_STATUS 
-#			WHEN 0 THEN 'Open'
-#			WHEN 1 THEN 'New'
-#			WHEN 2 THEN 'Closed'
-#			WHEN 3 THEN 'Open for Recovery'
-#			WHEN 4 THEN 'Void' ELSE CAST(CR.E87_STATUS AS varchar) END AS 'Feature Status'
-#		,CLASS.B83_DESCRIPTION AS 'Prof. Activity of Insured'
-#		,CR.[F09_DI_EXP_PAID] AS 'ITD Expense'
-#		,CR.E93_DI_LOSS AS 'Outstanding Loss Reserve'
-#		,CR.E99_DI_EXP AS 'Outstanding Expense Reserve'
-#		,CC.ACTIVITY_DESC AS 'Coverage Result/Activity Status'
-#		,FD.FILE_DISPOSITION_VALUE AS 'File Disposition'
-#		,'' AS 'Settlement Date'
-#		,CASE 
-#			WHEN CC.D76_ACTIVITY_TYPE = 'U' THEN 'Yes'
-#			WHEN CC.A13_ACT_STATUS = 'IS' THEN 'Yes'
-#			ELSE NULL END AS 'In-Suit Indicator'
-#
-#		,NULL AS 'Additional Notes'
-#		,NULL AS 'File Resolution'
-#		,NULL AS 'File Suffix'
-#		from RAW_PRIMIS.PRIMIS2.CCOMMON CC
-#		left join RAW_PRIMIS.PRIMIS2.CRESERVE CR
-#		ON CR.B69_CLAIM_OCCUR = CC.B69_CLAIM_OCCUR
-#		left join RAW_PRIMIS.PRIMIS2.PCOMMON PC
-#		ON PC.A00_PNUM = CC.A00_PNUM AND PC.A06_EDITION = CC.A06_EDITION
-#		left join RAW_PRIMIS.PRIMIS2.PCOVERAGE COV
-#		ON COV.A00_PNUM = PC.A00_PNUM AND COV.A06_EDITION = PC.A06_EDITION AND COV.B79_UNIT = CR.B79_UNIT AND COV.C87_COVERAGE = CR.C87_COVERAGE
-#		left join RAW_PRIMIS.PRIMIS2.PUNIT PU
-#		ON PU.A00_PNUM = PC.A00_PNUM AND PU.A06_EDITION = PC.A06_EDITION AND PU.B79_UNIT = CR.B79_UNIT
-#		left join RAW_PRIMIS.PRIMIS2.COMPANY CP
-#		ON CP.A01_COMPANY = CC.A01_COMPANY
-#		left join RAW_PRIMIS.PRIMIS2.NAME_ADDRESS NA1
-#		ON NA1.E04_ORIGNUM = CP.E04_NAMENUM AND NA1.E04_NEXT IS NULL
-#		left join RAW_PRIMIS.PRIMIS2.NAME_ADDRESS NA2
-#		ON NA2.E04_ORIGNUM = PC.E04_INS_ORIGNUM AND NA2.E04_NEXT IS NULL
-#		left join RAW_PRIMIS.PRIMIS2.NAME_ADDRESS ADJ
-#		ON ADJ.E04_ORIGNUM = CC.R30_IN_HOUSE_ADJ AND ADJ.E04_NEXT IS NULL
-#		left join RAW_PRIMIS.PRIMIS2.NAME_ADDRESS CLMT
-#		ON CLMT.E04_ORIGNUM = CR.E04_CLAIMANT_NUM AND CLMT.E04_NEXT IS NULL
-#		left join RAW_PRIMIS.PRIMIS2.LQ70_UNDERWRITER UW
-#		ON UW.Q70_UNDERWRITER = PC.Q70_UNDERWRITER
-#		left join RAW_PRIMIS.PRIMIS2.LB84_DED_TYPE DED
-#		ON DED.B84_DED_TYPE = COV.B84_DED_TYPE
-#		left join RAW_PRIMIS.PRIMIS2.LD76_ACTIVITY_TYPE AT
-#		ON AT.D76_ACTIVITY_TYPE = CC.D76_ACTIVITY_TYPE
-#		left join RAW_PRIMIS.PRIMIS2.LB83_CLASS_CODES CLASS
-#		ON CLASS.B83_CLASS = CR.B83_CLASS AND CLASS.B97_SUBLINE = CR.B97_SBL
-#		left join RAW_PRIMIS.PRIMIS2.FILE_DISPOSITION FD
-#		ON FD.FILE_DISPOSITION_ID = CC.FILE_DISPOSITION_ID
-#		WHERE 
-#		CC.B69_CLAIM_OCCUR LIKE 'SM%' OR CC.B69_CLAIM_OCCUR LIKE 'MM%' OR CC.B69_CLAIM_OCCUR LIKE 'LA%' OR CC.B69_CLAIM_OCCUR LIKE 'DO%'
-#
-#		
-#		) RESULTS
-#		"""
 
 
 
